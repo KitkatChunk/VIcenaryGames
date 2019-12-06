d3bd1941541215c3b1d577dd3cfce006cc9fe473
Exzeed <geethan1996@hotmail.ca>
2019-12-06 00:53:33 -0500

    Updated Transition
    Both players must now reach the end to progress

--- VIcenaryGames-master/Area 52/Assets/_Scripts/SceneTransition.cs
+++ VIcenaryGames-master/Area 52/Assets/_Scripts/SceneTransition.cs
@@ -2,16 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
     [SerializeField] public string nextLevel;
+    public Text coop1, coop2;
+    private bool isready, isready2, bothready;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.name == "Player 1")
         {
+            coop1.text = "Waiting for Player 2";
+            coop2.text = "Waiting for Player 2";
+            isready = true;
+        }
+        if (other.name == "Player 2")
+        {
+            coop1.text = "Waiting for Player 1";
+            coop2.text = "Waiting for Player 1";
+            isready2 = true;
+        }
+        if (isready && isready2)
+        {
+            coop1.text = "";
+            coop2.text = "";
             SceneManager.LoadScene(nextLevel);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player 1")
+        {
+            coop1.text = "";
+            coop2.text = "";
+            isready = false;
+        }
+        if (other.name == "Player 2")
+        {
+            coop1.text = "";
+            coop2.text = "";
+            isready2 = false;
         }
     }
 }
