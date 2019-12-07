f4994a3a8cfd05e45a45b093fb8a798917ad5fc9
Exzeed <geethan1996@hotmail.ca>
2019-12-07 14:13:27 -0500

    Added extra goal to level 2
    all enemies must be destroyed to progress, changed position values of hidden logs

---
+++ VIcenaryGames-master/Area 52/Assets/_Scripts/ClearedOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearedOut : MonoBehaviour
+{
+    public GameObject transition;
+    public Text rule1, rule2;
+    List<GameObject> listOfEnemies = new List<GameObject>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        NoEnemies();
+    }
+
+    private void NoEnemies()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 1)
+        {
+            rule1.text = "FIND THE FAR EXIT";
+            rule2.text = "FIND THE FAR EXIT";
+            transition.SetActive(true);
+        }
+    }
+}
